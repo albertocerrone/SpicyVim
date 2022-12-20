a7fcd9c614fa7f9fbef9bef421099339a2ae1857
Alberto Cerrone <cerrone.alberto93@icloud.com>
2022-12-20 23:03:44 +0100

    chore: Add TokyoNight

--- lua/albertocerrone/set.lua
+++ lua/albertocerrone/set.lua
@@ -31,7 +31,11 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+vim.cmd [[colorscheme tokyonight-night]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
+
+-- Set scrolloff
+vim.o.scrolloff = 8
+
