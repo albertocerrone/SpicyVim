5f3c84700d2bde175f69fdcbd9c52b93bba7325e
Alberto Cerrone <cerrone.alberto93@icloud.com>
2022-12-20 21:49:38 +0100

    refactor: Move files to be is own component
    BREAKING CHANGE:

---
+++ lua/albertocerrone/set.lua
@@ -0,0 +1,37 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Relative line numbers
+vim.o.nu = true
+vim.o.relativenumber = true
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 50
+vim.wo.signcolumn = 'yes'
+
+-- Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme onedark]]
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
