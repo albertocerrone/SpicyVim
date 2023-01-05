532e48015aa13254dfa01097d5d37c27ccfaa504
Alberto Cerrone <cerrone.alberto93@icloud.com>
2023-01-05 14:56:55 +0100

    feat: Very nice update to fugitive
    The keymap is going to open fugitive and wil place the cursor to
    the 5th line.

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,7 @@
-vim.keymap.set("n", "<leader>gt", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gt", function()
+    vim.cmd.Git()
+    vim.api.nvim_command("normal! 5j")
+end)
 
 local Alberto_Fugitive = vim.api.nvim_create_augroup("Alberto_Fugitive", {})
 
@@ -12,14 +15,14 @@ autocmd("BufWinEnter", {
         end
 
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
+        local opts = { buffer = bufnr, remap = false }
         vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git('push')
         end, opts)
 
         -- rebase always
         vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({'pull',  '--rebase'})
+            vim.cmd.Git({ 'pull', '--rebase' })
         end, opts)
 
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
