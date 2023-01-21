bfbcf26aa6e131c3a5baaf869c93ae80b8c30a1e
Alberto Cerrone <cerrone.alberto93@icloud.com>
2023-01-21 14:56:31 +0000

    feat: Ensure vim treesitter installed

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = {'css','lua', 'python', 'rust', 'typescript', 'help', 'html' },
+  ensure_installed = {'css','lua', 'python', 'rust', 'typescript', 'help', 'html', 'vim' },
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
