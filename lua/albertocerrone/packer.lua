3c1e7db6302c67ffe513b0d7c463c4b2e6612b36
Alberto Cerrone <cerrone.alberto93@icloud.com>
2023-01-26 19:01:23 +0000

    feat: Improve paker

--- lua/albertocerrone/packer.lua
+++ lua/albertocerrone/packer.lua
@@ -3,7 +3,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
-  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
   vim.cmd [[packadd packer.nvim]]
 end
 
@@ -19,6 +19,10 @@ require('packer').startup({ function(use)
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
+
+      -- Add linting support
+      "jose-elias-alvarez/null-ls.nvim",
+      "jay-babu/mason-null-ls.nvim",
 
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
