2c8cb5bf1fc5e8652b0f8b9eda0b2d52782dbbc6
Alberto Cerrone <cerrone.alberto93@icloud.com>
2023-02-27 09:04:44 +0000

    perf(lsp): Improve LSP performance

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,83 @@
+-- Keymaps
+local on_attach = function(_, bufnr)
+    -- NOTE: Remember that lua is a real programming language, and as such it is possible
+    -- to define small helper and utility functions so you don't have to repeat yourself
+    -- many times.
+    --
+    -- In this case, we create a function that lets us more easily define mappings specific
+    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = "LSP: " .. desc
+        end
+
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+    nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+    -- See `:help K` for why this keymap
+    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+    -- Lesser used LSP functionality
+    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+    nmap("<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, "[W]orkspace [L]ist Folders")
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+        vim.lsp.buf.format()
+    end, { desc = "Format current buffer with LSP" })
+end
+
 return {
+    {
+        "simrat39/rust-tools.nvim",
+        ft = { 'rust' },
+        opts = function ()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            return {
+            server = {
+                on_attach = on_attach,
+                capabilities = capabilities
+            },
+            tools = {
+                runnables = {
+                    use_telescope = true,
+                },
+                inlay_hints = {
+                    auto = true,
+                    show_parameter_hints = false,
+                    parameter_hints_prefix = "",
+                    other_hints_prefix = "",
+                },
+            },
+        }
+        end,
+        config = function(_, opts)
+            require("rust-tools").setup(opts)
+        end,
+        dependencies = {
+            'nvim-lspconfig',
+            -- Adds extra functionality over rust analyzer
+            {
+                'rust-lang/rust.vim',
+                ft = { 'rust' },
+            },
+        },
+    },
     { -- LSP Configuration & Plugins
         'neovim/nvim-lspconfig',
         event = { 'BufReadPre', 'BufNewFile' },
@@ -43,53 +122,8 @@ return {
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
             end
             vim.diagnostic.config(opts.diagnostics)
-            -- Keymaps
-            local on_attach = function(_, bufnr)
-                -- NOTE: Remember that lua is a real programming language, and as such it is possible
-                -- to define small helper and utility functions so you don't have to repeat yourself
-                -- many times.
-                --
-                -- In this case, we create a function that lets us more easily define mappings specific
-                -- for LSP related items. It sets the mode, buffer and description for us each time.
-                local nmap = function(keys, func, desc)
-                    if desc then
-                        desc = "LSP: " .. desc
-                    end
-
-                    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-                end
-
-                nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-                nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-                nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-                nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-                nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-                nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-                nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-                nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-                -- See `:help K` for why this keymap
-                nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-                nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-                -- Lesser used LSP functionality
-                nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-                nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-                nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-                nmap("<leader>wl", function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, "[W]orkspace [L]ist Folders")
-
-                -- Create a command `:Format` local to the LSP buffer
-                vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-                    vim.lsp.buf.format()
-                end, { desc = "Format current buffer with LSP" })
-            end
             local servers = opts.servers
-
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
             -- Ensure the servers above are installed
             local mason_lspconfig = require("mason-lspconfig")
@@ -98,12 +132,22 @@ return {
                 ensure_installed = vim.tbl_keys(servers),
             })
 
+            vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+            vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+            -- LSP settings (for overriding per client)
+            local handlers =  {
+                ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "single"}),
+                ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single" }),
+            }
+
             mason_lspconfig.setup_handlers({
                 function(server_name)
                     require("lspconfig")[server_name].setup({
                         capabilities = capabilities,
                         on_attach = on_attach,
                         settings = servers[server_name],
+                        handlers = handlers,
                     })
                 end,
             })
@@ -184,12 +228,6 @@ return {
         cmd = "Mason",
         config = true,
     },
-    -- Adds extra functionality over rust analyzer
-    {
-        'rust-lang/rust.vim',
-        ft = { 'rust' },
-    },
-    -- "simrat39/rust-tools.nvim",  # TODO: It depends on DAP and other tools
 }
 -- -- Configure LSP through rust-tools.nvim plugin.
 -- -- rust-tools will configure and enable certain LSP features for us.
@@ -240,4 +278,3 @@ return {
 --   },
 -- }
 --
--- require("rust-tools").setup(opts)
